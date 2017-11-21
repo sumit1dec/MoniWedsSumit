3344152f1ec2776ba89780731e482c04ca8b3da7
sumit1dec <sumit1dec@gmail.com>
2017-11-22 02:12:54 +0530

    changed photos. Made app compatible to Mobile view

--- public_html/js/customJS.js
+++ public_html/js/customJS.js
@@ -70,5 +70,20 @@ $(function () {
 });
 
 $(document).ready(function() {
-//    $("#myAudio").get(0).play();
+ $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
 });
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+     
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      
+  }
+}
